0ac2758fa3fb76ef953ed513910c83f5f3985e70
soumyajeetsengupta <soumyajeetsengupta@gmail.com>
2024-02-20 14:27:37 +0530

    Most optimal and Bruteforce solution added for GCD problem.

--- Maths/GCD.go
+++ Maths/GCD.go
@@ -11,6 +11,24 @@ func main() {
 	fmt.Print("Num2: ");
 	fmt.Scanf("%d\n", &num2);
 
+	// Bruteforce
+	var min int;
+	if num1 < num2 {
+		min = num1;
+	} else {
+		min = num2;
+	}
+
+	for i := 2; i <= min; i++ {
+		if num1 % i == 0 && num2 % i == 0 {
+			min = i;
+			break;
+		}
+	}
+
+	fmt.Println("The GCD is: ", min);
+
+	/* Most Optmial
 	for ;num1 % num2 != 0; {
 		var temp int = num1;
 		num1 = num2;
@@ -18,4 +36,5 @@ func main() {
 	}
 
 	fmt.Println("The GCD is: ", num2);
+	*/
 }

58738345581ce4a89dfba48a4a59ee9a06ee3493
Soumyajeet Sengupta <75798775+soumyajeetsengupta@users.noreply.github.com>
2024-02-20 08:24:01 -0500

    Fixes in GCD.go
    The bruteforce code had a failing testcase in previous code fixed it.

--- Maths/GCD.go
+++ Maths/GCD.go
@@ -12,17 +12,19 @@ func main() {
 	fmt.Scanf("%d\n", &num2);
 
 	// Bruteforce
-	var min int;
-	if num1 < num2 {
-		min = num1;
-	} else {
+	var min int = num1;
+	if num1 > num2 {
 		min = num2;
 	}
 
-	for i := 2; i <= min; i++ {
-		if num1 % i == 0 && num2 % i == 0 {
+	if (num1 == 0) && (num2 == 0) {
+		fmt.Println("The GCD is: ", 0);
+		return;
+	}
+
+	for i := 1; i <= min; i++ {
+		if (num1 % i == 0) && (num2 % i == 0) {
 			min = i;
-			break;
 		}
 	}
 

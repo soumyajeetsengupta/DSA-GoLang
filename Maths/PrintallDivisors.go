226bd40e302c996a5f736d3d4c2bcaeb22780df5
soumyajeetsengupta <soumyajeetsengupta@gmail.com>
2024-02-22 18:12:40 +0530

    files created

--- Maths/PrintallDivisors.go
+++ Maths/PrintallDivisors.go
@@ -6,14 +6,14 @@ func main() {
 	var n int = 10
 	var k int = 0
 	var divisor [5]int = [5]int{}
-
+	/*
 	for i := 1; i <= n/2; i++ {
 		if n%i == 0 {
 			divisor[k] = i
 			k += 1
 		}
 	}
-
+	*/
 	divisor[k] = n
 	fmt.Printf("Divisors of %d are: %d", n, divisor)
 }
